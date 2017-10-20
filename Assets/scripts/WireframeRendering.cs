8088fe017f23329eb1c55dc51bd14595db170fd0
Maria Murcia <mmaicrumaria@gmail.com>
2017-10-20 12:33:02 +0100

    Fixed demos to work with SteamVR. Added UCLA wireframe shader and spectator view demos.

---
+++ Assets/scripts/WireframeRendering.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireframeRendering : MonoBehaviour {
+    void OnPreRender()
+    {
+        GL.wireframe = true;
+    }
+    void OnPostRender()
+    {
+        GL.wireframe = false;
+    }
+}
